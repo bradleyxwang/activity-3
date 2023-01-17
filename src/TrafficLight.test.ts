df1e13df51f5c6e14dc7a20d978b62cf733b9a18
Karen <li.kar@northeastern.edu>
2023-01-17 13:18:40 -0500

    b

--- src/TrafficLight.test.ts
+++ src/TrafficLight.test.ts
@@ -1,15 +1,16 @@
 //import { Color, timerAtNextSecond, colorAtNextSecond } from './TrafficLight'
-import { Color, timerAtNextSecond, colorAtNextSecond } from './betterTrafficLight'
+import {timerAtNextSecond, colorAtNextSecond, Green, Yellow, Red } from './betterTrafficLight'
 
 test("tests for timerAtNextSecond", () => {
-    expect(timerAtNextSecond(17)).toBe(16)
-    expect(timerAtNextSecond(1)).toBe(20)
+    expect(timerAtNextSecond(17, 20)).toBe(16)
+    expect(timerAtNextSecond(1, 20)).toBe(20)
+    expect(timerAtNextSecond(4, 10)).toBe(3)
 })
 
 
 test("tests for TrafficLight", () => {
-    expect(colorAtNextSecond("red",7)).toEqual("red")
-    expect(colorAtNextSecond("red",1)).toEqual("green")
-    expect(colorAtNextSecond("green",1)).toEqual("yellow")
-    expect(colorAtNextSecond("yellow",1)).toEqual("red")
+    expect(colorAtNextSecond(Red,16)).toEqual("red")
+    expect(colorAtNextSecond(Red,1)).toEqual("green")
+    expect(colorAtNextSecond(Green,1)).toEqual("yellow")
+    expect(colorAtNextSecond(Yellow,1)).toEqual("red")
 })
--- src/betterTrafficLight.ts
+++ src/betterTrafficLight.ts
@@ -1,27 +1,18 @@
 export type Color = "red" | "green" | "yellow"
-type TrafficColor = {
+export type TrafficColor = {
     color : Color
     timeLength: number
     nextColor: Color 
 }
 
-const Red: TrafficColor = {color: 'red', timeLength: 20, nextColor: 'green'}
-const Yellow: TrafficColor = {color: 'yellow', timeLength: 10, nextColor: 'red'}
-const Green: TrafficColor = {color: 'green', timeLength: 20, nextColor: 'yellow'}
+export const Red: TrafficColor = {color: 'red', timeLength: 20, nextColor: 'green'}
+export const Yellow: TrafficColor = {color: 'yellow', timeLength: 10, nextColor: 'red'}
+export const Green: TrafficColor = {color: 'green', timeLength: 20, nextColor: 'yellow'}
 
 export function timerAtNextSecond (timer : number,  timerLength : number) : number {
     if (timer === 1) {return timerLength} return (timer - 1)
 }
 
-export function colorAtNextSecond(lightColor: Color, timer: number): Color {
-    switch (lightColor) {
-        case "red":
-            return (timer === 1) ? "green" : "red";
-        case "yellow":
-            return (timer === 1) ? "red" : "yellow";
-        case "green":
-            return (timer === 1) ? "yellow" : "green";
-        default:
-            throw new Error("This error shouldn't occur.")
-    }
+export function colorAtNextSecond(lightColor: TrafficColor, timer: number): Color {
+    return (timer === 1) ? lightColor.nextColor : lightColor.color
 }
